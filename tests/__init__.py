d82a0aa36476b88e78b90bd8a1019ed33dc0ba75
Andy Sinning <andrew.sinning@gmail.com>
2025-10-24 16:48:52 -0500

    first commit

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for AJS ERV Controller integration."""
---
+++ tests/const.py
@@ -0,0 +1,9 @@
+"""Constants for AJS ERV Controller tests."""
+from custom_components.ajs_erv_controller.const import (
+    CONF_PASSWORD,
+)
+from custom_components.ajs_erv_controller.const import (
+    CONF_USERNAME,
+)
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
